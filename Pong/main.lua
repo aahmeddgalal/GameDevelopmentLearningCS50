4194c0c699999f007423d9267783badd30ff44bf
Ledger <dr-jermy@outlook.com>
2026-04-26 00:51:26 +0300

    paddles movement

--- Pong/main.lua
+++ Pong/main.lua
@@ -4,6 +4,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 
 push = require 'push'
 
@@ -16,6 +18,9 @@ function love.load()
     player1score = 0
     player2score = 0
 
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = true,
         vsync = true,
@@ -27,6 +32,20 @@ push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 })
 
 end 
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt   
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
 
 function love.keypressed(key)
     if key == 'escape' then
@@ -45,10 +64,10 @@ function love.draw()
     love.graphics.print(tostring(player2score), VIRTUAL_WIDTH / 2 + 50, VIRTUAL_HEIGHT / 2 - 110)
 
     -- padle 1
-    love.graphics.rectangle('fill', 10, 10, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- padle 2
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     -- ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
